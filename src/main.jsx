a4bb52b9ec11b88b094682ae01e46b9572f52773
RN-Akash <akash@iquincesoft.com>
2025-06-26 18:25:45 +0530

    initial dash

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,13 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
-import Layout from './component/Layout.jsx';
 
-import Dashboard from './Dashboard.jsx';
-import About from './pages/About.jsx';
-import Contact from './pages/Contact.jsx';
+import Dash from './Dash.jsx';
 
-const AppComponent = Layout(Dashboard);
-const AboutComponent = Layout(About);
-const ContactComponent = Layout(Contact)
-
-const router = createBrowserRouter(
-  [
-    { path: '/', element: <AppComponent /> },
-    { path: '/about', element: <AboutComponent /> },
-    { path: '/contact', element: <ContactComponent /> },
-  ],
-);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Dash />
   </StrictMode>,
 );
